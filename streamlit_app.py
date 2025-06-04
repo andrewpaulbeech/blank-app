9278751811601f5150a2af7d1c66b99275a43baa
andrewpaulbeech <andy.beech@capgemini.com>
2025-06-04 11:05:27 +0000

    Commit Numbner 001

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new Steamlit app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building eggs! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
